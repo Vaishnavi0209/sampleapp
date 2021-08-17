36b69571517cbb6fd25cc237d133b9e9b493424e
vpotphod <vpotphod@LIN25005190.corp.capgemini.com>
2021-08-17 13:47:58 +0530

    Sales class  display method added with sum

--- sampleapp/src/main/java/sampleapp/SalesClass.java
+++ sampleapp/src/main/java/sampleapp/SalesClass.java
@@ -3,8 +3,14 @@ package sampleapp;
 public class SalesClass {
 	public void display() {
 		int[] arr = {23,45,65,67,6};
-		for(int num:arr)
+		int sum=0;
+		for(int num:arr) {
+			sum+=num;
 			System.out.println(num);
+		}
+		
+		System.out.println("Sumation:" + sum);
+			
 	}
 	
 	public void print() {
